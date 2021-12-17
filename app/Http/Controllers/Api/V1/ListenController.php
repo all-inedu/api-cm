c2bd648eed223e6e9005a7accf0181b46f100424
unknown <manuel.eric.b@gmail.com>
2021-12-17 17:19:00 +0700

    17/12/2021-2

--- app/Http/Controllers/Api/V1/ListenController.php
+++ app/Http/Controllers/Api/V1/ListenController.php
@@ -12,6 +12,7 @@ use App\Models\LastRead;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
 
 class ListenController extends Controller
@@ -19,17 +20,17 @@ class ListenController extends Controller
 
     public function getModuleContent($part_id, $group_id = "1")
     {
-
         $user = Auth::user();
         $user_id = $user->id;
 
-        $element = Element::with('elementdetails')
-                    ->where('group', $group_id)
-                    ->where('part_id', $part_id)->orderBy('order', 'asc')
+        $element = Element::query()->with(array('elementdetails' => function($query) {
+                        $query->select('*')->orderByRaw('RAND()');
+                    }))->where('group', $group_id)
+                    ->where('part_id', $part_id)
+                    ->orderBy('order', 'asc')
                     ->get();
 
-        $no = 1;
-        $big_data = $element;   
+
         foreach ($element as $key => $value) {
             $element_id = $value['id'];
             switch ($value['category_element']) {
@@ -52,11 +53,50 @@ class ListenController extends Controller
         
                     
         //! COUNT GROUP BY $part_id
-        $max_value_of_group = Element::where('part_id', $part_id)->max('group'); //jumlah group = jumlah pagex
+        $max_value_of_group = Element::where('part_id', $part_id)->max('group'); //jumlah group = jumlah page
+        $max_value_of_group = isset($max_value_of_group) ? $max_value_of_group : 1; 
         $current_page = $group_id;
         $next_page = $current_page + 1;
         $previous_page = $current_page - 1;
+        $next_outline_id = null;
 
+        $next_page_url = $current_page != $max_value_of_group ? URL::to('api/v1').'/listen/element/'.$part_id.'/'.$next_page : null;
+
+        //* THIS FUNCTION WILL RUN IF GROUP/PAGE HAS REACH THE END OF THE GROUP/PAGE OF THE PART
+        if ($current_page == $max_value_of_group) {
+
+            //! QUERY TO GET OUTLINE ID
+            $query_get_outline = DB::table('parts')->whereIn('outline_id', function ($query) use ($part_id) {
+                return $query->select('outline_id')->from('parts')->where('id', $part_id);
+            })->get();
+            
+            $outline_id = $query_get_outline[0]->outline_id;
+            $jumlah_part_id_in_outline = count($query_get_outline);
+            //! CHECK IF OUTLINE HAS MORE THAN 1 PART
+            // if ($jumlah_part_id_in_outline > 1) { //! IF JUMLAH PART BIGGER THAN 1
+                $query_get_next_part = DB::table('parts')->whereIn('id', function ($query) use ($part_id, $outline_id) {
+                    return $query->select(DB::raw('MIN(id)'))->from('parts')->where('id', '>', $part_id)->where('outline_id', $outline_id);
+                })->select('id as next_part_id')->first();
+
+                $next_part_id = isset($query_get_next_part->next_part_id) != NULL ? $query_get_next_part->next_part_id : $part_id;
+                $next_page_url = URL::to('api/v1').'/listen/element/'.$next_part_id.'/1';
+
+                //! IF NEXT PART ID WAS THE LATEST PART ON THIS OUTLINE_ID
+                if (empty($query_get_next_part->next_part_id)) {
+                    $query_get_next_outline = DB::table('outlines')->whereIn('id', function ($query) use ($outline_id) {
+                        return $query->select(DB::raw('MIN(id)'))->from('outlines')->where('id', '>', $outline_id);
+                    })->select('id as next_outline_id')->first();
+                    
+                    $next_outline_id = isset($query_get_next_outline->next_outline_id) ? $query_get_next_outline->next_outline_id : null;
+                }
+            // } else {
+            //     //! WHEN JUMLAH PART ID ONLY 1
+                
+            // }
+        }
+        //* END OF THE FUNCTION *//
+
+        //* START CREATE A PAGINATION *//
         $cst_pagination = array(
             'current_page' => $current_page,
             'data' => $element,
@@ -64,11 +104,12 @@ class ListenController extends Controller
             'from' => $current_page,
             'last_page' => $max_value_of_group,
             'last_page_url' => URL::to('api/v1').'/listen/element/'.$part_id.'/'.$max_value_of_group,
-            'next_page_url' => $current_page != $max_value_of_group ? URL::to('api/v1').'/listen/element/'.$part_id.'/'.$next_page : null,
+            'next_page_url' => $next_page_url,
             'path' => URL::to('api/v1').'/listen/element/'.$part_id.'/'.$current_page,
             'per_page' => null,
             'prev_page_url' => $current_page > 1 ? URL::to('api/v1').'/listen/element/'.$part_id.'/'.$previous_page : null,
-            'total' => $max_value_of_group
+            'total' => $max_value_of_group,
+            'next_outline_id' => $next_outline_id
         ); 
         
         //! GET LAST READ
@@ -106,10 +147,34 @@ class ListenController extends Controller
 
     public function getSectionDataBySlug($slug)
     {
+        $user = Auth::user();
+        $user_id = $user->id;
         $outline = Outline::select('outlines.id', 'outlines.name as outline_name', 'sections.id as section_id', 'sections.name as section_name')
                     ->rightJoin('sections', 'sections.id', '=', 'outlines.section_id')
                     ->join('modules', 'modules.id', '=', 'outlines.module_id')
                     ->where('slug', $slug)->get();
+
+        // $persentase_keseluruhan = 0;
+        // // $jumlah_part = 0;
+        // foreach($outline as $outline_item){
+        //     $part = Part::selectRaw('
+        //                 parts.id, parts.title as part_title, outlines.name as outline_name,
+        //                 (SELECT MAX(`group`) FROM elements WHERE part_id = parts.id) as total_group,
+        //                 (SELECT COUNT(*) FROM last_reads WHERE part_id = parts.id AND user_id = '.$user_id.') as total_read,
+        //                 (SELECT ROUND(total_read*100 / total_group)) as percentage
+        //                 ')
+        //         ->where('outline_id', $outline_item->id)
+        //         ->join('outlines', 'outlines.id', '=', 'parts.outline_id')
+        //         ->get();
+        //     $jumlah_part = count($part);
+            
+        //     foreach ($part as $part_item) {
+        //         $persentase_keseluruhan += $part_item->percentage;
+        //     }
+        // }
+
+        // return $persentase_keseluruhan." dan ".$jumlah_part;
+
         return response()->json(['success' => true, 'data' => compact('outline')], 200); 
     }
     
